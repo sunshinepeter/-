64109d396c15879e20dd96c12d94006fbea5836c
sunshinepeter <xiasong0803@163.com>
2015-11-12 19:14:46 +0800

    Update StringBuffer.java

--- StringBuffer.java
+++ StringBuffer.java
@@ -1,1 +1,16 @@
+package cn.com.jdzj.www
 
+public class StringBuffer{
+  /*数组，用于存储字符串*/
+  char[] value;
+  /* 用于标记字符串的长度*/
+  int count;
+  
+  /**
+   *构造函数：这里提供3种 
+   */
+   public StringBuffer(){
+    
+   }
+   
+}
